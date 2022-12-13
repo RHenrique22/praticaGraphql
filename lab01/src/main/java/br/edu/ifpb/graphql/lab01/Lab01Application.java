9f5ad75dc558b929f94bd58bf6efac0da28fec3f
RHenrique <roosevelthenrique22@gmail.com>
2022-12-13 00:44:14 -0300

    [fix]: Consertado detalhes despercebidos

--- lab01/src/main/java/br/edu/ifpb/graphql/lab01/Lab01Application.java
+++ lab01/src/main/java/br/edu/ifpb/graphql/lab01/Lab01Application.java
@@ -27,7 +27,7 @@ public class Lab01Application implements CommandLineRunner {
 	}
 
 	@Override
-	public void run(String... args) throws Exception {
+	public void run( String... args ) throws Exception {
 
 		Author a1 = new Author( 
 			1, 
@@ -59,13 +59,13 @@ public class Lab01Application implements CommandLineRunner {
 		Book   	b2 = new Book( 2, "Memórias Póstumas de Brás Cubas", 320, a1 );
 		Book   	b3 = new Book( 3, "Quincas Borba", 270, a1 );
 
-		Book	b4 = new Book(4, "Os Sertões", 496 , a2);
-		Book	b5 = new Book(5, "Canudos (diario de uma expedição)", 184, a2);
-		Book	b6 = new Book(6, "Amazônia", 261 , a2);
+		Book	b4 = new Book( 4, "Os Sertões", 496 , a2 );
+		Book	b5 = new Book( 5, "Canudos (diario de uma expedição)", 184, a2 );
+		Book	b6 = new Book( 6, "Amazônia", 261 , a2 );
 
-		Book	b7 = new Book(7, "Uma pedra no meio do caminho", 194, a3);
-		Book	b8 = new Book(8, "Vasto mundo", 168 , a3);
-		Book	b9 = new Book(9, "Brejo das almas", 80 , a3);
+		Book	b7 = new Book( 7, "Uma pedra no meio do caminho", 194, a3 );
+		Book	b8 = new Book( 8, "Vasto mundo", 168 , a3 );
+		Book	b9 = new Book( 9, "Brejo das almas", 80 , a3 );
 
 		List<Book> books = new ArrayList<>();
 
